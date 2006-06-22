511c2d3bc99b4a3fdf3c9b18a876beae2c585773
Milos Kleint <mkleint@gmail.com>
2006-06-22 17:13:10 +0000

    initial contribution of nbm-maven-plugin, plugin for handling netbeans module projects.
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/nbm-maven-plugin@1987 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/main/java/org/codehaus/mojo/nbm/ExamineManifest.java
@@ -0,0 +1,282 @@
+/* ==========================================================================
+ * Copyright 2003-2004 Mevenide Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * =========================================================================
+ */
+package org.codehaus.mojo.nbm;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * Tag examines the manifest of a jar file and retrieves netbeans specific information.
+ * @author <a href="mailto:mkleint@codehaus.org">Milos Kleint</a>
+ *
+ */
+public class ExamineManifest  {
+    
+    private File jarFile;
+    private File manifestFile;
+    // package private to simplify testing
+    private boolean netbeansModule;
+    private boolean localized;
+    private String specVersion;
+    private String implVersion;
+    private String module;
+    private String moduleDeps;
+    private String locBundle;
+    private boolean publicPackages;
+    private boolean populateDependencies = false;
+    private List dependencyTokens = Collections.EMPTY_LIST;
+   
+    
+    public void checkFile() throws MojoExecutionException {
+        
+        resetExamination();
+        
+        Manifest mf = null;
+        if (jarFile != null) {
+            JarFile jar = null;
+            try {
+                jar = new JarFile(jarFile);
+                mf = jar.getManifest();
+            } catch (Exception exc) {
+                throw new MojoExecutionException( exc.getMessage(), exc );
+            } finally {
+                if (jar != null) {
+                    try {
+                        jar.close();
+                    } catch (IOException io) {
+                        throw new MojoExecutionException( io.getMessage(), io );
+                    }
+                }
+            }
+        } else if (manifestFile != null) {
+            InputStream stream = null;
+            try {
+                stream = new FileInputStream(manifestFile);
+                mf = new Manifest(stream);
+            } catch (Exception exc) {
+                throw new MojoExecutionException( exc.getMessage(), exc );
+            } finally {
+                if (stream != null) {
+                    try {
+                        stream.close();
+                    } catch (IOException io) {
+                        throw new MojoExecutionException( io.getMessage(), io );
+                    }
+                }
+            }
+        }
+        if (mf != null) {
+            processManifest(mf);
+        } else {
+            throw new MojoExecutionException("Cannot read jar file or manifest file");
+        }
+    }
+    
+    void resetExamination() {
+        setNetbeansModule(false);
+        setLocalized(false);
+        setSpecVersion(null);
+        setImplVersion(null);
+        setModule(null);
+        setModuleDeps(null);
+        setLocBundle(null);
+        setPublicPackages(false);
+    }
+    
+    void processManifest(Manifest mf) {
+        Attributes attrs = mf.getMainAttributes();
+        setModule(attrs.getValue("OpenIDE-Module"));
+        setNetbeansModule(getModule() != null);
+        if (isNetbeansModule()) {
+            setLocBundle(attrs.getValue("OpenIDE-Module-Localizing-Bundle"));
+            setLocalized((getLocBundle() == null ? false : true));
+            setSpecVersion(attrs.getValue("OpenIDE-Module-Specification-Version"));
+            setImplVersion(attrs.getValue("OpenIDE-Module-Implementation-Version"));
+            setModuleDeps(attrs.getValue("OpenIDE-Module-Module-Dependencies"));
+            String value = attrs.getValue("OpenIDE-Module-Public-Packages");
+            if (attrs.getValue("OpenIDE-Module-Friends") != null || value == null || value.trim().equals("-")) {
+                setPublicPackages(false);
+            } else {
+                setPublicPackages(true);
+            }
+            if (isPopulateDependencies()) {
+                String deps = attrs.getValue("OpenIDE-Module-Module-Dependencies");
+                if (deps != null) {
+                    StringTokenizer tokens = new StringTokenizer(deps, ",");
+                    List depList = new ArrayList();
+                    while (tokens.hasMoreTokens()) {
+                        String tok = tokens.nextToken();
+                        //we are just interested in specification and loose dependencies.
+                        int spec = tok.indexOf(">");
+                        if (spec > 0 || (tok.indexOf("=") == -1 && spec == -1)) {
+                            if (spec > 0) {
+                                tok = tok.substring(0, spec - 1);
+                            }
+                            int slash = tok.indexOf("/");
+                            if (slash > 0) {
+                                tok = tok.substring(0, slash - 1);
+                            }
+                            depList.add(tok.trim());
+                        }
+                    }
+                    setDependencyTokens(depList);
+                }
+            }
+            
+        } else {
+            // for non-netbeans jars.
+            setSpecVersion(attrs.getValue("Specification-Version"));
+            setImplVersion(attrs.getValue("Implementation-Version"));
+            setModule(attrs.getValue("Package"));
+            setPublicPackages(false);
+        /*    if (module != null) {
+                // now we have the package to make it a module definition, add the version there..
+                module = module + "/1"; 
+            }
+         */
+            if (getModule() == null) {
+                // do we want to do that?
+                setModule(attrs.getValue("Extension-Name"));
+            }
+        }
+        
+    }
+    
+    /**
+     * Getter for property jarFile.
+     * @return Value of property jarFile.
+     */
+    public java.io.File getJarFile() {
+        return jarFile;
+    }
+
+    /**
+     * The jar file to examine. It is exclusing with manifestFile.
+     */
+    public void setJarFile(java.io.File jarFileLoc) {
+        jarFile = jarFileLoc;
+    }
+
+    
+    /** Getter for property manifestFile.
+     * @return Value of property manifestFile.
+     *
+     */
+    public File getManifestFile()
+    {
+        return manifestFile;
+    }    
+    
+    /** 
+     * Manifest file to be examined. It is exclusing with jarFile.
+     */
+    public void setManifestFile(File manifestFileLoc)
+    {
+        manifestFile = manifestFileLoc;
+    }
+
+    public boolean isNetbeansModule() {
+        return netbeansModule;
+    }
+
+    public void setNetbeansModule(boolean netbeansModule) {
+        this.netbeansModule = netbeansModule;
+    }
+
+    public boolean isLocalized() {
+        return localized;
+    }
+
+    public void setLocalized(boolean localized) {
+        this.localized = localized;
+    }
+
+    public String getSpecVersion() {
+        return specVersion;
+    }
+
+    public void setSpecVersion(String specVersion) {
+        this.specVersion = specVersion;
+    }
+
+    public String getImplVersion() {
+        return implVersion;
+    }
+
+    public void setImplVersion(String implVersion) {
+        this.implVersion = implVersion;
+    }
+
+    public String getModule() {
+        return module;
+    }
+
+    public void setModule(String module) {
+        this.module = module;
+    }
+
+    public String getModuleDeps() {
+        return moduleDeps;
+    }
+
+    public void setModuleDeps(String moduleDeps) {
+        this.moduleDeps = moduleDeps;
+    }
+
+    public String getLocBundle() {
+        return locBundle;
+    }
+
+    public void setLocBundle(String locBundle) {
+        this.locBundle = locBundle;
+    }
+
+    public boolean hasPublicPackages() {
+        return publicPackages;
+    }
+
+    public void setPublicPackages(boolean publicPackages) {
+        this.publicPackages = publicPackages;
+    }
+
+    public boolean isPopulateDependencies() {
+        return populateDependencies;
+    }
+
+    public void setPopulateDependencies(boolean populateDependencies) {
+        this.populateDependencies = populateDependencies;
+    }
+
+    public List getDependencyTokens() {
+        return dependencyTokens;
+    }
+
+    public void setDependencyTokens(List dependencyTokens) {
+        this.dependencyTokens = dependencyTokens;
+    }
+    
+}
---
+++ src/main/java/org/codehaus/mojo/nbm/PopulateRepositoryMojo.java
@@ -0,0 +1,393 @@
+/* ==========================================================================
+ * Copyright 2003-2006 Mevenide Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * =========================================================================
+ */
+package org.codehaus.mojo.nbm;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.deployer.ArtifactDeployer;
+import org.apache.maven.artifact.deployer.ArtifactDeploymentException;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.installer.ArtifactInstallationException;
+import org.apache.maven.artifact.installer.ArtifactInstaller;
+import org.apache.maven.artifact.metadata.ArtifactMetadata;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
+import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
+import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.artifact.ProjectArtifactMetadata;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.Input;
+import org.apache.tools.ant.taskdefs.PathConvert;
+import org.apache.tools.ant.types.FileSet;
+
+
+/**
+ * a goal for identifying netbeans modules from the installation and populationg the local
+ * repository with them. Optionally you can also deploy to a remote repository.
+   <p/>
+ * If you are looking for an existing remote repository for netbeans artifacts, check out
+ * http://208.44.201.216:18080/maven/ it contains API artifacts for 4.1 and 5.0 releases.
+ 
+ * @author <a href="mailto:mkleint@codehaus.org">Milos Kleint</a>
+ * @goal populate-repository
+ * @requiresProject false
+ * @aggregator
+ */
+public class PopulateRepositoryMojo
+        extends AbstractNbmMojo {
+    /**
+     * Local maven repository.
+     *
+     * @parameter expression="${localRepository}"
+     * @required
+     * @readonly
+     */
+    protected ArtifactRepository localRepository;
+
+    /**
+     * an url where to deploy the netbeans artifacts.. optional, if not specified, the artifacts will be only installed
+     * in local repository
+     * @parameter expression="${deployUrl}"
+     */
+    private String deployUrl;
+    
+    /**
+     * Location of netbeans installation
+     *
+     * @parameter expression="${netbeansInstallDirectory}"
+     */
+    
+    protected String netbeansInstallDirectory;
+    
+    
+    /**
+     * If you want to install/deploy also netbeans api javadocs, download the javadoc zip file from netbeans.org
+     * expand it to a directory, it should contain multiple zip files. Define this parameter as absolute path to the zip files folder.
+     *
+     * @parameter expression="${netbeansJavadocDirectory}"
+     */
+    protected String netbeansJavadocDirectory;
+    
+    /**
+     * Optional parameter, when specified, will force all modules to have the designated version.
+     * Good when depending on releases. Then you would for example specify RELEASE50 in this parameter and
+     * all modules get this version in the repository.
+     * @parameter expression="${forcedVersion}"
+     */
+    protected String forcedVersion;
+    
+    /**
+     * Maven ArtifactFactory.
+     *
+     * @parameter expression="${component.org.apache.maven.artifact.factory.ArtifactFactory}"
+     * @required
+     * @readonly
+     */
+    private ArtifactFactory artifactFactory;
+    
+    /**
+     * Maven ArtifactInstaller.
+     *
+     * @parameter expression="${component.org.apache.maven.artifact.installer.ArtifactInstaller}"
+     * @required
+     * @readonly
+     */
+    private ArtifactInstaller artifactInstaller;
+    
+    
+    /**
+     * Maven ArtifactDeployer.
+     *
+     * @parameter expression="${component.org.apache.maven.artifact.deployer.ArtifactDeployer}"
+     * @required
+     * @readonly
+     */
+    private ArtifactDeployer artifactDeployer;
+    
+    /**
+     * Maven ArtifactRepositoryFactory.
+     *
+     * @parameter expression="${component.org.apache.maven.artifact.repository.ArtifactRepositoryFactory}"
+     * @required
+     * @readonly
+     */
+    private ArtifactRepositoryFactory repositoryFactory;
+    
+    
+    public void execute() throws MojoExecutionException {
+        getLog().info("Populate repository with netbeans modules");
+        Project antProject = registerNbmAntTasks();
+        ArtifactRepository deploymentRepository = null;
+        if (deployUrl != null) {
+            ArtifactRepositoryLayout layout = new DefaultRepositoryLayout();
+            deploymentRepository = repositoryFactory.createDeploymentArtifactRepository("netbeans", deployUrl, layout, true);
+        }
+        
+        
+        if (netbeansInstallDirectory == null) {
+            Input input = (Input)antProject.createTask("input");
+            input.setMessage("Please enter netbeans installation directory:");
+            input.setAddproperty("installDir");
+            try {
+                input.execute();
+            } catch (BuildException e) {
+                getLog().error( "Cannot run ant:input" );
+                throw new MojoExecutionException( e.getMessage(), e );
+            }
+            String prop = antProject.getProperty("installDir");
+            netbeansInstallDirectory = prop;
+        }
+        
+        File rootDir = new File(netbeansInstallDirectory);
+        if (!rootDir.exists()) {
+            getLog().error("Netbeans installation doesn't exist.");
+            throw new MojoExecutionException("Netbeans installation doesn't exist.");
+        }
+        getLog().info("Copying Netbeans artifacts from " + netbeansInstallDirectory);
+        
+        PathConvert convert = (PathConvert)antProject.createTask("pathconvert");
+        convert.setPathSep(",");
+        convert.setProperty("netbeansincludes");
+        FileSet set = new FileSet();
+        set.setDir(rootDir);
+        set.createInclude().setName("**/modules/*.jar");
+        set.createInclude().setName("**/modules/autoload/*.jar");
+        set.createInclude().setName("**/modules/eager/*.jar");
+        set.createInclude().setName("platform*/core/*.jar");
+        set.createInclude().setName("platform*/lib/*.jar");
+        
+        convert.createPath().addFileset(set);
+        try {
+            convert.execute();
+        } catch (BuildException e) {
+            getLog().error( "Cannot run ant:pathconvert" );
+            throw new MojoExecutionException( e.getMessage(), e );
+        }
+        
+        String prop = antProject.getProperty("netbeansincludes");
+        StringTokenizer tok = new StringTokenizer(prop, ",");
+        HashMap moduleDefinitions = new HashMap();
+        while (tok.hasMoreTokens()) {
+            String token = tok.nextToken();
+            File module = new File(token);
+            ExamineManifest examinator = new ExamineManifest();
+            examinator.setPopulateDependencies(true);
+            examinator.setJarFile(module);
+            examinator.checkFile();
+            if (examinator.isNetbeansModule()) {
+                //TODO get artifact id from the module's manifest?
+                String artifact = module.getName().substring(0, module.getName().indexOf(".jar"));
+                String version = forcedVersion == null ? examinator.getSpecVersion() : forcedVersion;
+                String group = "org.netbeans." + (examinator.hasPublicPackages() ? "api" : "modules");
+                String m = examinator.getModule();
+                int slash = m.indexOf("/");
+                if (slash > 0) {
+                    m = m.substring(0, slash - 1);
+                }
+                m = m.trim();
+                examinator.setModule(m);
+                Artifact art = createArtifact(artifact, version, group);
+                moduleDefinitions.put(new ModuleWrapper(artifact, version, group, examinator, module), art);
+            }
+        }
+        List wrapperList = new ArrayList(moduleDefinitions.keySet());
+        Iterator it = moduleDefinitions.entrySet().iterator();
+        File javadocRoot = null;
+        if (netbeansJavadocDirectory != null) {
+            javadocRoot = new File(netbeansJavadocDirectory);
+            if (!javadocRoot.exists()) {
+                javadocRoot = null;
+                throw new MojoExecutionException("The netbeansJavadocFolder parameter doesn't point to an existing folder");
+            }
+        }
+        while (it.hasNext()) {
+            Map.Entry elem = (Map.Entry) it.next();
+            ModuleWrapper man = (ModuleWrapper)elem.getKey();
+            Artifact art = (Artifact)elem.getValue();
+            
+            File pom = createMavenProject(man, wrapperList);
+            ArtifactMetadata metadata = new ProjectArtifactMetadata(art, pom);
+            art.addMetadata( metadata );
+            File javadoc = null;
+            Artifact javadocArt = null;
+            if (javadocRoot != null) {
+                File zip = new File(javadocRoot, art.getArtifactId() + ".zip");
+                if (zip.exists()) {
+                    javadoc = zip;
+                    javadocArt = artifactFactory.createArtifactWithClassifier(art.getGroupId(), 
+                                                                              art.getArtifactId(),
+                                                                              art.getVersion(),
+                                                                              "jar", "javadoc");
+                }
+            }
+            
+            try {
+                if (javadoc != null) {
+                    artifactInstaller.install(javadoc,javadocArt, localRepository);
+                }
+                artifactInstaller.install(man.getFile(), art, localRepository );
+            } catch ( ArtifactInstallationException e ) {
+                // TODO: install exception that does not give a trace
+                throw new MojoExecutionException( "Error installing artifact", e );
+            }
+            try {
+                if (deploymentRepository != null) {
+                    if (javadoc != null) {
+                        artifactDeployer.deploy(javadoc, javadocArt, deploymentRepository, localRepository);
+                    }
+                    artifactDeployer.deploy(man.getFile(), art, deploymentRepository, localRepository);
+                }
+            } catch (ArtifactDeploymentException ex) {
+                throw new MojoExecutionException( "Error Deploying artifact", ex );
+            }
+            
+        }
+    }
+    
+    
+    
+    
+    
+    private File createMavenProject(ModuleWrapper wrapper, List wrapperList) {
+        Model mavenModel = new Model();
+        
+        mavenModel.setGroupId(wrapper.getGroup());
+        mavenModel.setArtifactId(wrapper.getArtifact());
+        mavenModel.setVersion(wrapper.getArtifact());
+        mavenModel.setPackaging("jar");
+        mavenModel.setModelVersion("4.0.0");
+        ExamineManifest man = wrapper.getModuleManifest();
+        if (!man.getDependencyTokens().isEmpty()) {
+            List deps = new ArrayList();
+            Iterator it = man.getDependencyTokens().iterator();
+            while (it.hasNext()) {
+                String elem = (String) it.next();
+                // create pseudo wrapper
+                ModuleWrapper wr = new ModuleWrapper(elem);
+                int index = wrapperList.indexOf(wr);
+                if (index > -1) {
+                    wr = (ModuleWrapper)wrapperList.get(index);
+                    //we don't want the API modules to depend on non-api ones..
+                    // otherwise the transitive dependency mechanism pollutes your classpath..
+                    if ((wr.getModuleManifest().hasPublicPackages() && wrapper.getModuleManifest().hasPublicPackages())
+                                 || !wrapper.getModuleManifest().hasPublicPackages()) {
+                        Dependency dep = new Dependency();
+                        dep.setArtifactId(wr.getArtifact());
+                        dep.setGroupId(wr.getGroup());
+                        dep.setVersion(wr.getVersion());
+                        dep.setType("jar");
+                        deps.add(dep);
+                    }
+                } else {
+                    getLog().warn("No module found for dependency '" + elem + "'");
+                }
+            }
+            mavenModel.setDependencies(deps);
+        }
+        FileWriter writer = null;
+        File fil = null;
+        try {
+            MavenXpp3Writer xpp = new MavenXpp3Writer();
+            fil = File.createTempFile("maven", "pom");
+            writer = new FileWriter(fil);
+            xpp.write(writer, mavenModel);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException io) {
+                    io.printStackTrace();
+                }
+            }
+        }
+        return fil;
+    }
+    
+    
+    private Artifact createArtifact(String artifact, String version, String group) {
+        return artifactFactory.createBuildArtifact(group, artifact, version, "jar");
+    }
+    
+    private class ModuleWrapper  {
+        ExamineManifest man;
+        private String artifact;
+        private String version;
+        private String group;
+        private File file;
+
+        String module;
+        
+        public ModuleWrapper(String module) {
+            this.module = module;
+        }
+        
+        public ModuleWrapper(String art, String ver, String grp, ExamineManifest manifest, File fil) {
+            man = manifest;
+            artifact = art;
+            version = ver;
+            group = grp;
+            file = fil;
+        }
+        
+        public int hashCode() {
+            return getModule().hashCode();
+        }
+        
+        public boolean equals(Object obj) {
+            return getModule().equals(((ModuleWrapper)obj).getModule());
+        }
+        
+        public String getModule() {
+            return module != null ? module : getModuleManifest().getModule();
+        }
+        
+        public ExamineManifest getModuleManifest() {
+            return man;
+        }
+        
+        private String getArtifact() {
+            return artifact;
+        }
+        
+        private String getVersion() {
+            return version;
+        }
+        
+        private String getGroup() {
+            return group;
+        }
+
+        private File getFile() {
+            return file;
+        }
+    }
+}
