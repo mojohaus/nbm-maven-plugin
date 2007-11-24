1d7537cd71b08f6b147dbf66f26aad7aa7c0bf12
Milos Kleint <mkleint@gmail.com>
2007-11-24 19:15:08 +0000

    mockup toolchains support classes, code commented out.
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/nbm-maven-plugin@5638 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/main/java/org/codehaus/mojo/nbm/toolchain/DefaultNetbeansToolchain.java
@@ -0,0 +1,62 @@
+/*
+ *  Copyright 2007 mkleint.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+package org.codehaus.mojo.nbm.toolchain;
+
+//import java.util.Map;
+//import org.apache.maven.toolchain.DefaultToolchain;
+
+/**
+ *
+ * @author mkleint
+ */
+public class DefaultNetbeansToolchain /*extends DefaultToolchain implements NetbeansToolchain */ {
+
+//    private String installDir;
+//    public static final String KEY_INSTALL_DIR = "installDir"; //NOI18N
+//
+//    public DefaultNetbeansToolchain() {
+//        super("netbeans");
+//    }
+//    
+//    public String getInstallDir() {
+//        return installDir;
+//    }
+//
+//    public void setInstallDir(String installDir) {
+//        this.installDir = installDir;
+//    }
+//
+//    public String toString() {
+//        return "NetBeans[" + getInstallDir() + "]";
+//    }
+//    
+//    
+//    public String findTool(String toolName) {
+//        throw new IllegalStateException();
+//    }
+//
+//    public Map getData() {
+//        Map map = super.getData();
+//        map.put(KEY_INSTALL_DIR, installDir);
+//        return map;
+//    }
+//
+//    public void setData(Map data) {
+//        super.setData(data);
+//        installDir = (String)data.get(KEY_INSTALL_DIR);
+//    }
+}
