8f2d8a6cff43930135779606dfa112dcb7f3d1de
Milos Kleint <mkleint@gmail.com>
2008-09-30 13:28:42 +0000

    accomodate the nbm:run-platform goal to the nbm-application packaging only
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/nbm-maven-plugin@7768 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/codehaus/mojo/nbm/RunPlatformAppMojo.java
+++ src/main/java/org/codehaus/mojo/nbm/RunPlatformAppMojo.java
@@ -17,12 +17,6 @@
 package org.codehaus.mojo.nbm;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.regex.Pattern;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -33,11 +27,10 @@ import org.codehaus.plexus.util.cli.Commandline;
 import org.codehaus.plexus.util.cli.StreamConsumer;
 
 /**
- * Run a branded application on top of NetBeans Platform with additional custom module clusters, 
- * to be used in conjunction with nbm:cluster.
+ * Run a branded application on top of NetBeans Platform. To be used with projects
+ * with nbm-application packaging only and the project needs to be built first.
  * @author <a href="mailto:mkleint@codehaus.org">Milos Kleint</a>
  * @goal run-platform
- * @aggregator
  * @requiresDependencyResolution runtime
  *
  */
@@ -46,25 +39,18 @@ public class RunPlatformAppMojo
 {
 
     /**
-     * directory where the module(s)' netbeans cluster(s) are located.
-     * is related to nbm:cluster goal.
-     * @parameter default-value="${project.build.directory}/netbeans_clusters"
-     * @required
-     */
-    protected String clusterBuildDir;
-    /**
-     * directory where the the NetBeans platform/IDE installation is,
-     * denotes the root directory of netbeans installation.
-     * @parameter expression="${netbeans.installation}"
-     * @required
-     */
-    protected String netbeansInstallation;
-    /**
      * The branding token for the application based on NetBeans platform.
      * @parameter expression="${netbeans.branding.token}"
      * @required
      */
     protected String brandingToken;
+    /**
+     * output directory where the the netbeans application is created.
+     * @parameter default-value="${project.build.directory}"
+     * @required
+     */
+    private File buildDirectory;
+
     /**
      * netbeans user directory for the executed instance.
      * @parameter default-value="${project.build.directory}/userdir" expression="${netbeans.userdir}"
@@ -78,31 +64,6 @@ public class RunPlatformAppMojo
      * @parameter expression="${netbeans.run.params}"
      */
     protected String additionalArguments;
-    /**
-     * List of enabled clusters. At least platform cluster needs to be
-     * included.
-     * @parameter
-     * @required
-     */
-    protected List/*<String>*/ enabledClusters;
-//    /**
-//     * List of disabled modules in the enabled clusters. 
-//     * Allows for fine-tuned configuration of the application.
-//     * @parameter
-//     */
-//    protected List/*<String>*/ disabledModules;
-//    
-//   TODO no support for disabling individual modules,
-//        should look something like this..   
-//    
-//        <createmodulexml xmldir="${cluster}/config/Modules">
-//            <hidden dir="${netbeans.dest.dir}">
-//                <custom classpath="${harness.dir}/tasks.jar" classname="org.netbeans.nbbuild.ModuleSelector">
-//                    <param name="excludeModules" value="${disabled.modules}"/>
-//                    <param name="excluded" value="true"/>
-//                </custom>
-//            </hidden>
-//        </createmodulexml>
     /**
      * The Maven Project.
      *
@@ -119,122 +80,25 @@ public class RunPlatformAppMojo
      */
     public void execute() throws MojoExecutionException, MojoFailureException
     {
+        if ( !"nbm-application".equals( project.getPackaging() ) )
+        {
+            throw new MojoFailureException( "The nbm:run-platform goal shall be used within a NetBeans Application project only ('nbm-application' packaging)");
+        }
+
         File userDir = new File( netbeansUserdir );
         userDir.mkdirs();
 
-        File clusterRoot = new File( clusterBuildDir );
-        if ( !clusterRoot.exists() )
-        {
-            throw new MojoExecutionException(
-                    "There are no additional clusters in " + clusterBuildDir );
-        }
+        File appbasedir = new File( buildDirectory, brandingToken );
 
-        // make sure the final cluster list is numbered.
-        Map matchers = new HashMap();
-        if ( enabledClusters == null )
-        {
-            // fallback default.
-            enabledClusters = new ArrayList();
+        if (!appbasedir.exists()) {
+            throw new MojoExecutionException( "The directory that shall contain built application, doesn't exist (" + appbasedir.getAbsolutePath()  + ")\n Please invoke 'mvn install' on the project first");
         }
-        Iterator it = enabledClusters.iterator();
-        while ( it.hasNext() )
-        {
-            String clus = (String) it.next();
-            // we don't want the plaform in the list of clusters..
-            if ( !clus.startsWith( "platform" ) )
-            {
-                matchers.put( clus, Pattern.compile( clus + "(\\d)*" ) );
-            }
-        }
-
-        /** list of files **/
-        List realEnabledClusters = new ArrayList();
-        File platform = null;
-        // check installation..
-        File[] fls = new File( netbeansInstallation ).listFiles();
-        if ( fls == null )
-        {
-            //null means not existing folder
-            throw new MojoExecutionException(
-                    "Non-existing NetBeans installation at " + netbeansInstallation );
-        }
-        for ( int i = 0; i < fls.length; i++ )
-        {
-            if ( fls[i].isDirectory() )
-            {
-                String folderName = fls[i].getName();
-                if ( folderName.startsWith( "platform" ) )
-                {
-                    platform = fls[i];
-                }
-                Iterator it2 = matchers.entrySet().iterator();
-                while ( it2.hasNext() )
-                {
-                    Map.Entry en = (Map.Entry) it2.next();
-                    Pattern match = (Pattern) en.getValue();
-                    if ( match.matcher( folderName ).matches() )
-                    {
-                        realEnabledClusters.add( fls[i] );
-                        it2.remove();
-                        break;
-                    }
-                }
-            }
-        }
-
-        // check our module clusters.
-        fls = clusterRoot.listFiles();
-        for ( int i = 0; i < fls.length; i++ )
-        {
-            if ( fls[i].isDirectory() )
-            {
-                String folderName = fls[i].getName();
-                Iterator it2 = matchers.entrySet().iterator();
-                while ( it2.hasNext() )
-                {
-                    Map.Entry en = (Map.Entry) it2.next();
-                    Pattern match = (Pattern) en.getValue();
-                    if ( match.matcher( folderName ).matches() )
-                    {
-                        // add our clusters at the start..
-                        realEnabledClusters.add( 0, fls[i] );
-                        it2.remove();
-                        break;
-                    }
-                }
-            }
-        }
-
-        if ( platform == null )
-        {
-            throw new MojoExecutionException(
-                    "Cannot find platform* cluster within NetBeans installation at " + netbeansInstallation );
-        }
-        // add all we could not match in platform and cross fingers.
-        if ( matchers.size() > 0 )
-        {
-            getLog().error( "Cannot find following clusters, ignoring:" );
-            it = matchers.keySet().iterator();
-            while ( it.hasNext() )
-            {
-                getLog().error( "      " + it.next() );
-            }
-        }
-
-        // create cluster list;
-        it = realEnabledClusters.iterator();
-        String clustersString = "";
-        while ( it.hasNext() )
-        {
-            clustersString = clustersString + File.pathSeparator + ((File) it.next()).getAbsolutePath();
-        }
-        clustersString = clustersString.substring( 1 );
 
         boolean windows = Os.isFamily( "windows" );
 
         Commandline cmdLine = new Commandline();
-        File exec = windows ? new File( platform, "lib\\nbexec.exe" ) : new File(
-                platform, "lib/nbexec" );
+        File exec = windows ? new File( appbasedir, "bin\\" + brandingToken + ".exe" )
+                            : new File( appbasedir, "bin/" + brandingToken );
         cmdLine.setExecutable( exec.getAbsolutePath() );
 
         try
@@ -247,9 +111,7 @@ public class RunPlatformAppMojo
                 "-J-Dnetbeans.logger.console=true",
                 "-J-ea",
                 "--branding",
-                brandingToken,
-                "--clusters",
-                Commandline.quoteArgument( clustersString )
+                brandingToken
             };
             cmdLine.addArguments( args );
             cmdLine.addArguments( Commandline.translateCommandline(
